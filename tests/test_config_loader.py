21f09f5656a2d114e64a9cb132570732f86c471a
Adam Schilperoort <adamschilperoort@arizona.edu>
2025-02-06 08:15:35 -0700

    make ConfigLoader a class accepting arguments

--- tests/test_config_loader.py
+++ tests/test_config_loader.py
@@ -1,18 +1,11 @@
 import pytest
-from config_pearl import config_raw, config_parsed, config_values_only
+from config_pearl import ConfigLoader
 
-# add tests for each toml file
-# currently testing for value existence, not anything useful 
-def test_raw_config():
-    assert "observatory" in config_raw
-    assert "telescope" in config_raw["observatory"]
-    assert "fnum" in config_raw["observatory"]["telescope"]
+loader = ConfigLoader("config_pearl/","raw",recursive=True) 
+config_parsed = loader.load_configs()
 
 def test_parsed_config():
     assert isinstance(config_parsed["observatory"]["telescope"]["jitter_rms"], dict)
     assert "value" in config_parsed["observatory"]["telescope"]["jitter_rms"]
     assert "unit" in config_parsed["observatory"]["telescope"]["jitter_rms"]
-
-def test_values_only_config():
-    assert isinstance(config_values_only["observatory"]["telescope"]["fnum"], float)
 
