c3ede917a1a77fb59d178e1efa6898ad89d66254
Adam Schilperoort <adamschilperoort@arizona.edu>
2025-02-18 09:09:54 -0700

    move support_data and configs into nested dir (#14)

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import importlib.metadata
-from config_pearl import load_config_values, get_data_path
-
-__version__ = importlib.metadata.version(__package__ or "config_pearl")
-
-__all__ = ["load_config_values", "get_data_path"]
