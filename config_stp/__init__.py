be45cf6a4738b3c27ec91c7ee1d6efe794aa40bd
Adam Schilperoort <adamschilperoort@arizona.edu>
2025-02-27 09:23:27 -0700

    adamschilperoort/update_data_path (#30)
    * update paths and configs
    
    * return a path with a '/' at the end for get_data_path() method

--- config_stp/__init__.py
+++ config_stp/__init__.py
@@ -11,7 +11,7 @@ def get_data_path():
     if not data_path.exists():
         raise FileNotFoundError(f"Support data directory not found: {data_path}")
 
-    return str(data_path)
+    return str(data_path) + "/"
 
 __all__ = ["load_config_values", "get_data_path", "__version__"]
 
