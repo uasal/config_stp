7d02401bb1e3dbb0b325af0d8346041d3cca0866
Adam Schilperoort <adamschilperoort@arizona.edu>
2025-04-03 15:04:09 -0700

    working on json output from toml configs

---
+++ scripts/config_to_json.py
@@ -0,0 +1,16 @@
+import sys
+import json 
+from pathlib import Path
+import config_stp
+
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT / "src"))
+
+def main():
+    output_path = ROOT / "outputs" / "config_stp.json"
+    data = config_stp.load_config_values('parsed')
+    with output_path.open("w") as f:
+        json.dump(data, f, indent=2)
+
+if __name__ == "__main__":
+    main()
